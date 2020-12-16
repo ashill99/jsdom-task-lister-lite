0404dc31f2ae48a45c56a5b455c7e5d39ed411e6
ashill99 <admshng@gmail.com>
2020-12-16 17:39:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let newForm = document.querySelector("form#create-task-form")
+    newForm.addEventListener("submit", function (evt) {
+      evt.preventDefault()
+      console.log("form submitted")
+      let newTask = document.getElementById("new-task-description")
+      let toDoTask = newTask.value 
+      console.log(toDoTask)
+      let ul = document.querySelector("ul#tasks")
+      let li = document.createElement("li")
+      li.textContent = toDoTask
+      ul.append(li)
+      evt.target.reset()
+    })
 });
+
